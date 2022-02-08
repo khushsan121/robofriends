87062da32453bfb91ba2f31b9ab4be393ca663be
Khushwant <khush.san121@gmail.com>
2022-02-08 23:09:14 +0530

    uploading all files

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Card = ({name,email, id}) =>{
+
+	return(
+		<div className="tc bg-light-green dib pa3 br3 ma2 grow bw-2 shadow-5" >
+		 <img src={`https://robohash.org/${id}?size=200x200`} />
+
+		 <div>
+			<h2>{name}</h2>
+			<p>{email}</p>
+
+	     </div>
+		</div>	
+		);
+}
+export default Card;
